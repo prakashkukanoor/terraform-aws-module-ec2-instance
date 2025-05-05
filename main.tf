76e86e89d99be85055fcba9d5f9f16e18e5e850a
prakashkukanoor <prakashkop054@gmail.com>
2025-05-05 14:24:21 -0400

    root module setup

---
+++ main.tf
@@ -0,0 +1,27 @@
+locals {
+  common_tags = {
+    environment = var.environment
+    managedBy   = var.team
+    createdBy   = "terraform"
+  }
+}
+
+data "aws_ami" "this" {
+  most_recent = true
+  owners      = ["amazon"]
+
+  filter {
+    name   = "name"
+    values = [var.filter_name]
+  }
+}
+
+resource "aws_instance" "this" {
+  ami           = data.this.id
+  instance_type = var.instance_type
+  subnet_id     = var.subnet_id
+
+  tags = merge(
+    local.common_tags,
+  { Name = "EC2-${var.environment}" })
+}
---
+++ variables.tf
@@ -0,0 +1,15 @@
+variable "environment" {
+  type = string
+}
+variable "team" {
+  type = string
+}
+variable "instance_type" {
+  type = string
+}
+variable "subnet_id" {
+  type = string
+}
+variable "filter_name" {
+  type = string
+}
